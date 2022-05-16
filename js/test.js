8a6418a2d3434085fde8eef2d2692a401c41df8e
Jay <63338983+NirmithVictor@users.noreply.github.com>
2022-05-16 09:56:48 +0300

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -2,6 +2,18 @@ const theLinks = document.querySelectorAll(".nav-item");
 theLinks.forEach(link => link.addEventListener("click", ()=>{
     document.getElementById("checkie").checked=false;
 }))
+
+let spinnerWrapper = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    spinnerWrapper.style.display = "none";
+    const theLinks = document.querySelectorAll(".nav-item");
+    theLinks.forEach(link => link.addEventListener("click", ()=>{
+        document.getElementById("checkie").checked=false;
+    }))
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Charles%20Leclerc&prop=pageimages&format=json&pithumbsize=100&formatversion=2`).then(res=>res.json()).then((content)=>{
+        console.log(content)
+    })
+})
 /*
 
 WE GET THIS DATA FROM THIS API
@@ -39,3 +51,13 @@ console.log(a.query)
 console.log(a.query.pages)
 console.log(a.query.pages[0])
 console.log(a.query.pages[0].pageimage)*/
+/*
+We need to strip the name from the initial data of drivers to get their image since it is causing alot of issues
+in terms of naming convention such as first and name last 
+I have tested this code to scrap out our champ's datas and it worked
+We can strip the name from the wikipedia default link by simply
+
+var a="http://en.wikipedia.org/wiki/Nico_H%C3%BClkenberg"
+a=a.replace("http://en.wikipedia.org/wiki/",'')
+-> to get 'Nico_H%C3%BClkenberg'
+*/
